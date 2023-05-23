e6043b0d6f1493f21e883c4f70bed9d5593cf76b
aya-s-design <85773832+aya-s-design@users.noreply.github.com>
2023-05-23 11:44:46 +0900

    Add files via upload

---
+++ fes-site/js/hamburger.js
@@ -0,0 +1,9 @@
+$(".hamburger-btn").click(function(){
+    $(this).toggleClass('active');
+    $(".sp-nav").toggleClass('panelactive');
+});
+
+$(".sp-nav a").click(function(){
+    $(".hamburger-btn").removeClass('active');
+    $(".sp-nav").removeClass('panelactive');
+});
