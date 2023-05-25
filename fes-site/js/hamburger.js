6c4ec5ed631ca10943da711577485cd09decfa91
aya-s-design <85773832+aya-s-design@users.noreply.github.com>
2023-05-25 23:34:16 +0900

    Delete fes-site directory

--- fes-site/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-$(".hamburger-btn").click(function(){
-    $(this).toggleClass('active');
-    $(".sp-nav").toggleClass('panelactive');
-});
-
-$(".sp-nav a").click(function(){
-    $(".hamburger-btn").removeClass('active');
-    $(".sp-nav").removeClass('panelactive');
-});
